a3f862b3789028ce7eaa80182ea56eb4eaacaaa1
Damodar Yadav <115637298+MrLEGENDx10@users.noreply.github.com>
2024-02-04 04:19:30 +0530

    ff

--- server/app.js
+++ server/app.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(morgan('dev'));
 app.use(cors({
-    origin:'http://localhost:7000',
+    origin:[process.env.FRONTEND_URL],
     credentials:true
 }));
 
